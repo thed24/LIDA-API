947e11ee1eb0241619f720de13f0eb24a2ae0dbd
thed24 <domcodespoti@gmail.com>
2021-09-03 14:40:21 +1000

    Allow cors because unsafe and lazy

--- index.ts
+++ index.ts
@@ -12,6 +12,13 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(cors({ origin: true }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 RegisterRoutes(app);
 
