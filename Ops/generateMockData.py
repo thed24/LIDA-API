c3e9aa233b7700c0434e077c241b04a5af8a6153
thed24 <domcodespoti@gmail.com>
2021-09-18 14:58:45 +1000

    Clean up script

--- Ops/generateMockData.py
+++ Ops/generateMockData.py
@@ -20,16 +20,18 @@ sensors = [
 ]
 
 def create_sensor_data_payload(sensor, value):
-    return {"sensorName": sensor, "value:": value}
+    return {"sensorName": sensor, "value": value}
 
 for i in range(1, 100):
     print("Creating system log {}".format(i))
     mock_data = create_system_log_payload(random.choice(system_logs))
+    print(mock_data)
     response = requests.post(f'{base_url}/SystemLog', mock_data)
     print(response.text)
 
 for i in range(1, 100):
     print("Creating sensor data {}".format(i))
-    mock_data = create_sensor_data_payload(random.choice(sensors), random.randint(1, 100))
+    mock_data = create_sensor_data_payload(random.choice(sensors), str(random.randint(1, 100)))
+    print(mock_data)
     response = requests.post(f'{base_url}/SensorData', mock_data)
     print(response.text)
